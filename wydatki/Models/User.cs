7fd4a2f2e7dfce85ea0c175dae1278a28552f8b9
Hinerp <08filipkomorowski@gmail.com>
2025-05-26 17:00:12 +0200

    Added user class

---
+++ wydatki/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace wydatki.Models;
+
+public class User
+{
+    private int Id { get; set; }
+
+    private string Name { get; set; }
+    
+    private string Surname { get; set; }
+
+    private double Funds { get; set; }
+}
